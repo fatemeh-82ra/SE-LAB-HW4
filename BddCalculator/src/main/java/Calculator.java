3d3d967a721cc41e3a3531ecd9e30e0f500afc80
fatemeh <fati20khoji@gmail.com>
2025-08-08 13:36:47 +0330

    Added core logic for + operation

---
+++ BddCalculator/src/main/java/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+
+    public Double calculate(Double operand1, Double operand2, String operator) {
+        switch (operator) {
+            case "+":
+                return operand1 + operand2;
+
+            default:
+                // Handle cases where an unknown operator is used
+                throw new IllegalArgumentException("Invalid operator: " + operator);
+        }
+    }
+}
